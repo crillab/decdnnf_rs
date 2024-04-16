df6077cc4f6e0c4f096f5e685d91c4e7df395cd4
emmanuel lonca <lonca@cril.fr>
2024-04-16 17:32:31 +0200

    added some options to model enumeration app

--- src/app/model_enumeration.rs
+++ src/app/model_enumeration.rs
@@ -1,7 +1,9 @@
 use super::common;
 use crusti_app_helper::{info, App, AppSettings, Arg, SubCommand};
-use decdnnf_rs::{BottomUpTraversal, CheckingVisitor, ModelEnumerator};
-use std::io::{BufWriter, Write};
+use decdnnf_rs::{
+    BottomUpTraversal, CheckingVisitor, DecisionDNNF, Literal, ModelEnumerator, ModelFinder,
+};
+use std::io::{BufWriter, StdoutLock, Write};
 
 #[derive(Default)]
 pub struct Command;
@@ -9,6 +11,8 @@ pub struct Command;
 const CMD_NAME: &str = "model-enumeration";
 
 const ARG_COMPACT_FREE_VARS: &str = "ARG_COMPACT_FREE_VARS";
+const ARG_DECISION_TREE: &str = "ARG_DECISION_TREE";
+const ARG_DO_NOT_PRINT: &str = "ARG_DO_NOT_PRINT";
 
 impl<'a> crusti_app_helper::Command<'a> for Command {
     fn name(&self) -> &str {
@@ -29,58 +33,178 @@ impl<'a> crusti_app_helper::Command<'a> for Command {
                     .takes_value(false)
                     .help("compact models with free variables"),
             )
+            .arg(
+                Arg::with_name(ARG_DECISION_TREE)
+                    .long("decision-tree")
+                    .takes_value(false)
+                    .conflicts_with(ARG_COMPACT_FREE_VARS)
+                    .help("enumerate by building a decision tree (should be less efficient)"),
+            )
+            .arg(
+                Arg::with_name(ARG_DO_NOT_PRINT)
+                    .long("do-not-print")
+                    .takes_value(false)
+                    .help("do not print the models (for testing purpose)"),
+            )
     }
 
     fn execute(&self, arg_matches: &crusti_app_helper::ArgMatches<'_>) -> anyhow::Result<()> {
-        let ddnnf = common::read_input_ddnnf(arg_matches)?;
-        let traversal_visitor = Box::<CheckingVisitor>::default();
-        let traversal_engine = BottomUpTraversal::new(traversal_visitor);
-        let checking_data = traversal_engine.traverse(&ddnnf);
-        common::print_warnings_and_errors(&checking_data)?;
-        let mut n_enumerated = 0;
-        let mut n_models = 0;
-        let mut model_iterator =
-            ModelEnumerator::new(&ddnnf, arg_matches.is_present(ARG_COMPACT_FREE_VARS));
-        let mut sign_location = Vec::with_capacity(ddnnf.n_vars());
+        if arg_matches.is_present(ARG_DECISION_TREE) {
+            enum_decision_tree(arg_matches)
+        } else {
+            enum_default(arg_matches)
+        }
+    }
+}
+
+fn enum_default(arg_matches: &crusti_app_helper::ArgMatches<'_>) -> anyhow::Result<()> {
+    let ddnnf = load_ddnnf(arg_matches)?;
+    let mut model_writer = ModelWriter::new(
+        ddnnf.n_vars(),
+        arg_matches.is_present(ARG_COMPACT_FREE_VARS),
+        arg_matches.is_present(ARG_DO_NOT_PRINT),
+    );
+    let mut model_iterator =
+        ModelEnumerator::new(&ddnnf, arg_matches.is_present(ARG_COMPACT_FREE_VARS));
+    while let Some(model) = model_iterator.compute_next_model() {
+        model_writer.write_model_ordered(model);
+    }
+    model_writer.finalize();
+    Ok(())
+}
+
+fn enum_decision_tree(arg_matches: &crusti_app_helper::ArgMatches<'_>) -> anyhow::Result<()> {
+    let ddnnf = load_ddnnf(arg_matches)?;
+    let mut model_writer = ModelWriter::new(
+        ddnnf.n_vars(),
+        arg_matches.is_present(ARG_COMPACT_FREE_VARS),
+        arg_matches.is_present(ARG_DO_NOT_PRINT),
+    );
+    let model_finder = ModelFinder::new(&ddnnf);
+    if let Some(model) = model_finder.find_model() {
+        enum_decision_tree_from(&model_finder, &mut vec![], &model, &mut model_writer);
+    }
+    model_writer.finalize();
+    Ok(())
+}
+
+fn enum_decision_tree_from(
+    model_finder: &ModelFinder,
+    assumptions: &mut Vec<Literal>,
+    last_model: &[Literal],
+    model_writer: &mut ModelWriter,
+) {
+    if assumptions.len() == last_model.len() {
+        model_writer.write_model_no_opt(last_model);
+        return;
+    }
+    let new_var_in_model = *last_model
+        .iter()
+        .find(|l| l.var_index() == assumptions.len())
+        .unwrap();
+    assumptions.push(new_var_in_model);
+    enum_decision_tree_from(model_finder, assumptions, last_model, model_writer);
+    assumptions.pop();
+    assumptions.push(new_var_in_model.flip());
+    if let Some(model) = model_finder.find_model_under_assumptions(assumptions) {
+        enum_decision_tree_from(model_finder, assumptions, &model, model_writer);
+    }
+    assumptions.pop();
+}
+
+fn load_ddnnf(arg_matches: &crusti_app_helper::ArgMatches<'_>) -> anyhow::Result<DecisionDNNF> {
+    let ddnnf = common::read_input_ddnnf(arg_matches)?;
+    let traversal_visitor = Box::<CheckingVisitor>::default();
+    let traversal_engine = BottomUpTraversal::new(traversal_visitor);
+    let checking_data = traversal_engine.traverse(&ddnnf);
+    common::print_warnings_and_errors(&checking_data)?;
+    Ok(ddnnf)
+}
+
+struct ModelWriter {
+    pattern: Vec<u8>,
+    sign_location: Vec<usize>,
+    buf: BufWriter<StdoutLock<'static>>,
+    n_enumerated: usize,
+    n_models: usize,
+    compact_display: bool,
+    do_not_print: bool,
+}
+
+impl ModelWriter {
+    fn new(n_vars: usize, compact_display: bool, do_not_print: bool) -> Self {
+        let mut sign_location = Vec::with_capacity(n_vars);
         let mut pattern = Vec::new();
         pattern.push(b'v');
-        for i in 1..=ddnnf.n_vars() {
+        for i in 1..=n_vars {
             pattern.push(b' ');
             sign_location.push(pattern.len());
             pattern.push(b' ');
             pattern.extend_from_slice(format!("{i}").as_bytes());
         }
         pattern.extend_from_slice(" 0 \n".as_bytes());
-        let stdout = std::io::stdout();
-        let lock = stdout.lock();
-        let mut buf = BufWriter::with_capacity(128 * 1024, lock);
-        while let Some(model) = model_iterator.compute_next_model() {
-            n_enumerated += 1;
-            let mut current_n_models = 1;
-            model
-                .iter()
-                .zip(sign_location.iter())
-                .for_each(|(opt_l, o)| {
-                    if let Some(l) = opt_l {
-                        if l.polarity() {
-                            pattern[*o] = b' ';
-                        } else {
-                            pattern[*o] = b'-';
-                        }
+        Self {
+            pattern,
+            sign_location,
+            buf: BufWriter::with_capacity(128 * 1024, std::io::stdout().lock()),
+            n_enumerated: 0,
+            n_models: 0,
+            compact_display,
+            do_not_print,
+        }
+    }
+
+    fn write_model_ordered(&mut self, model: &[Option<Literal>]) {
+        self.n_enumerated += 1;
+        if self.do_not_print {
+            self.n_models += 1 << model.iter().filter(|opt| opt.is_none()).count();
+            return;
+        }
+        let mut current_n_models = 1;
+        model
+            .iter()
+            .zip(self.sign_location.iter())
+            .for_each(|(opt_l, o)| {
+                if let Some(l) = opt_l {
+                    if l.polarity() {
+                        self.pattern[*o] = b' ';
                     } else {
-                        pattern[*o] = b'*';
-                        current_n_models <<= 1;
+                        self.pattern[*o] = b'-';
                     }
-                });
-            let _ = buf.write_all(&pattern);
-            n_models += current_n_models;
+                } else {
+                    self.pattern[*o] = b'*';
+                    current_n_models <<= 1;
+                }
+            });
+        let _ = self.buf.write_all(&self.pattern);
+        self.n_models += current_n_models;
+    }
+
+    fn write_model_no_opt(&mut self, model: &[Literal]) {
+        self.n_enumerated += 1;
+        self.n_models += 1;
+        if self.do_not_print {
+            return;
         }
-        buf.flush().unwrap();
-        if arg_matches.is_present(ARG_COMPACT_FREE_VARS) {
-            info!("enumerated {n_enumerated} compact models corresponding to {n_models} models");
+        for l in model {
+            if l.polarity() {
+                self.pattern[self.sign_location[l.var_index()]] = b' ';
+            } else {
+                self.pattern[self.sign_location[l.var_index()]] = b'-';
+            }
+        }
+        let _ = self.buf.write_all(&self.pattern);
+    }
+
+    fn finalize(mut self) {
+        self.buf.flush().unwrap();
+        if self.compact_display {
+            info!(
+                "enumerated {} compact models corresponding to {} models",
+                self.n_enumerated, self.n_models
+            );
         } else {
-            info!("enumerated {n_enumerated} models");
+            info!("enumerated {} models", self.n_enumerated);
         }
-        Ok(())
     }
 }

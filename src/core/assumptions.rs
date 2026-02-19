c9a579aaa7f2463f8dd74fd993884b0c54d45684
emmanuel lonca <lonca@cril.fr>
2026-02-19 13:49:19 +0100

    forbid redefinition of assumptions

--- src/core/assumptions.rs
+++ src/core/assumptions.rs
@@ -1,6 +1,5 @@
-use std::ops::Index;
-
 use crate::Literal;
+use std::ops::Index;
 
 /// A structure that is used to handle literal assumptions.
 #[derive(Debug, Clone)]
@@ -17,10 +16,15 @@ impl Assumptions {
     /// # Panics
     ///
     /// This method will panic if the index of a literal is equal to or greater than the number of variables.
+    /// It will also panic is more than one literal references the same variable.
     #[must_use]
     pub fn new(n_vars: usize, assumptions: Vec<Literal>) -> Self {
         let mut mapping = vec![None; n_vars];
         for a in &assumptions {
+            assert!(
+                mapping[a.var_index()].is_none(),
+                "assumption set multiple times"
+            );
             mapping[a.var_index()] = Some(a.polarity());
         }
         Self {
@@ -41,5 +45,34 @@ impl Index<usize> for Assumptions {
 
     fn index(&self, index: usize) -> &Self::Output {
         &self.mapping[index]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ok() {
+        let assumption_lits = vec![Literal::from(2), Literal::from(-3)];
+        let assumptions = Assumptions::new(4, assumption_lits.clone());
+        assert_eq!(assumptions.as_slice(), assumption_lits.as_slice());
+        assert_eq!(None, assumptions[0]);
+        assert_eq!(Some(true), assumptions[1]);
+        assert_eq!(Some(false), assumptions[2]);
+        assert_eq!(None, assumptions[3]);
+    }
+
+    #[test]
+    #[should_panic(expected = "index out of bounds: the len is 4 but the index is 4")]
+    fn test_out_of_bounds() {
+        let assumptions = Assumptions::new(4, vec![]);
+        let _ = assumptions[4];
+    }
+
+    #[test]
+    #[should_panic(expected = "assumption set multiple times")]
+    fn test_redefinition() {
+        let _ = Assumptions::new(4, vec![Literal::from(1), Literal::from(1)]);
     }
 }
